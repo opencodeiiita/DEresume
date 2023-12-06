435483cb87a5a4535358058b04670824b76a415d
archief2910 <128996033+archief2910@users.noreply.github.com>
2023-12-06 11:55:27 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ require('dotenv').config(); //all the key value pairs are being made available due to this lib
+ require("@nomicfoundation/hardhat-toolbox");
+  
+ module.exports = {
+   solidity: "0.8.17",
+   defaultNetwork: 'sepolia',
+   networks: {
+     hardhat: {},
+     sepolia: {
+         url: process.env.API_URL_KEY,
+         accounts: [`0x${process.env.PRIVATE_KEY}`]
+     }
+   }
+ };
