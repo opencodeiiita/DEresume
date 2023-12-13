0c2cac9ee9d273e66fe089ea138ec5e27ad2675b
Sagnik Mandal <acriticalcynic@outlook.com>
2023-12-14 05:12:11 +0530

    Added Insititute Contract

--- smart-contracts/institutes.sol
+++ smart-contracts/institutes.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
+contract Institutes {
+    struct Institute {
+        string name;
+        string description;
+    }
+
+    mapping(address => Institute) public institutes;
+
+    function registerInstitute(string memory name, string memory description) public {
+        Institute memory newInstitute = Institute({
+            name: name,
+            description: description
+        });
+
+        institutes[msg.sender] = newInstitute;
+    }
+
+    function getInstitute(address instituteAddress) public view returns (Institute memory) {
+        return institutes[instituteAddress];
+    }
+}
