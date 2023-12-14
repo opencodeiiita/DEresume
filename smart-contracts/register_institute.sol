d07226c97b9467ae3bf48b1bd125fa45d05530c3
Rohit Pandey <120464365+Rohit-110@users.noreply.github.com>
2023-12-14 18:22:24 +0530

    Update register_institute.sol

--- smart-contracts/register_institute.sol
+++ smart-contracts/register_institute.sol
@@ -9,20 +9,21 @@ contract InstituteRegistration {
 
     mapping(address => Institute) public institutes;
 
-    event InstituteRegistered(address indexed walletAddress, string name, string description);
-
     function isRegisteredInstitute(address walletAdd) external view returns (bool) {
         return bytes(institutes[walletAdd].name).length > 0;
     }
 
-    function registerInstitute(string memory _name, string memory _description) external {
+    function registerInstitute(string memory _name, string memory _description) public {
         require(bytes(_name).length > 0, "Name must be filled");
         require(bytes(_description).length > 0, "Description must be filled");
         require(bytes(institutes[msg.sender].name).length == 0, "Address is already registered");
 
-        Institute memory newInstitute = Institute(_name, _description);
-        institutes[msg.sender] = newInstitute;
+        
+        Institute memory newInstitute = Institute({
+            name: _name,
+            description: _description
+        });
+        institutes[msg.sender]=newInstitute;
 
-        emit InstituteRegistered(msg.sender, _name, _description);
     }
 }
