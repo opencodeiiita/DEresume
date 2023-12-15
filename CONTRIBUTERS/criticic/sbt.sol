222352592c02739c2692c39d0e8de818f1280704
Sagnik Mandal <acriticalcynic@outlook.com>
2023-12-16 02:41:54 +0530

    restrict to registered institutes

--- CONTRIBUTERS/criticic/sbt.sol
+++ CONTRIBUTERS/criticic/sbt.sol
@@ -11,8 +11,11 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../../smart-contracts/institutes.sol";
 
 contract SBT is ERC721URIStorage {
+    Institutes institutes;
+
     struct Skill {
         string experienceType;
         string title;
@@ -23,7 +26,9 @@ contract SBT is ERC721URIStorage {
 
     mapping(uint256 => Skill) public skills;
 
-    constructor() ERC721("Soul-Bound Token", "SBT") {}
+    constructor(address institutesAddress) ERC721("SBT", "Soul-Bound-Tokens") {
+        institutes = Institutes(institutesAddress);
+    }
 
     function mint(
         address to,
@@ -34,6 +39,7 @@ contract SBT is ERC721URIStorage {
         string memory startDate,
         string memory endDate
     ) public {
+        require(institutes.isInstitute(msg.sender), "Only registered institutes can mint SBTs");
         _mint(to, tokenId);
 
         Skill memory newSkill = Skill({
