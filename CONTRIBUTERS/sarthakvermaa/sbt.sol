5ac67fd480348c54c86b32756e901b7fcd28b634
Sarthak Verma <114106172+sarthakvermaa@users.noreply.github.com>
2023-12-16 16:49:36 +0530

    Update sbt.sol

--- CONTRIBUTERS/sarthakvermaa/sbt.sol
+++ CONTRIBUTERS/sarthakvermaa/sbt.sol
@@ -4,20 +4,59 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract SBToken is ERC721URIStorage {
+    struct Skill {
+        string experienceType;
+        string skillTitle;
+        string skillDescription;
+        uint256 startDate;
+        uint256 endDate;
+        // Add more parameters as needed for the skill
+    }
+
+    mapping(uint256 => Skill) private tokenIdToSkill;
+    mapping(uint256 => bool) private skillTransferable;
+
     uint256 private tokenIdCounter;
 
     constructor() ERC721("Soul-Bound Token", "SBT") {}
 
-    function mintToken(address recipient, string memory tokenURI) external returns (uint256) {
+    function mintToken(
+        address recipient,
+        string memory tokenURI,
+        string memory expType,
+        string memory title,
+        string memory description,
+        uint256 start,
+        uint256 end
+    ) external returns (uint256) {
         uint256 newTokenId = tokenIdCounter;
         _safeMint(recipient, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
+
+        Skill memory newSkill = Skill({
+            experienceType: expType,
+            skillTitle: title,
+            skillDescription: description,
+            startDate: start,
+            endDate: end
+            // Add more parameters initialization if needed
+        });
+
+        tokenIdToSkill[newTokenId] = newSkill;
+        skillTransferable[newTokenId] = false; // Skill transferability set to false initially
         tokenIdCounter++;
+
         return newTokenId;
     }
 
+    function setSkillTransferable(uint256 tokenId, bool transferable) external {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "SBToken: Caller is not owner nor approved");
+        skillTransferable[tokenId] = transferable;
+    }
+
     function transferToken(address to, uint256 tokenId) external {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+        require(skillTransferable[tokenId], "SBToken: Token is not transferable");
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "SBToken: Caller is not owner nor approved");
         _safeTransfer(_msgSender(), to, tokenId, "");
     }
 
@@ -27,5 +66,9 @@ contract SBToken is ERC721URIStorage {
 
     function getTokenURI(uint256 tokenId) external view returns (string memory) {
         return tokenURI(tokenId);
+    }
+
+    function getSkill(uint256 tokenId) external view returns (Skill memory) {
+        return tokenIdToSkill[tokenId];
     }
 }
